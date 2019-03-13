4412fa2937bc4030d63260d611aa160e07bd8bdb
Alexis <38560748+Frozenmouth@users.noreply.github.com>
2019-03-13 17:17:39 +0100

    Create js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+popup = function() {
+      if (confirm("Hey le nouveau site xxxvidsxxx est trop bien, viens dessus stp please")) {
+            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      }
+}
+window.setTimeout(popup, 10000)
